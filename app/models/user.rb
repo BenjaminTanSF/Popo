1cf0bc203d0d0cdd2e545766f7460e98c23419cd
BenjaminTan <yiren.tan@gmail.com>
2019-07-03 10:03:16 -0700

    Complete backend auth

---
+++ app/models/user.rb
@@ -0,0 +1,62 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  position        :string           not null
+#  org_id          :integer          not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  # credit: https://stackoverflow.com/questions/5123972/ruby-on-rails-password-validation
+  # PASSWORD_FORMAT = /\A
+  #   (?=.{8,})          # Must contain 8 or more characters
+  #   (?=.*\d)           # Must contain a digit
+  #   (?=.*[a-z])        # Must contain a lower case character
+  #   (?=.*[A-Z])        # Must contain an upper case character
+  #   (?=.*[[:^alnum:]]) # Must contain a symbol
+  # /x
+
+  attr_reader :password
+
+  validates :username, :password_digest, :email, :first_name, :last_name, :position, :org_id, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  # validates :password, format: { with: PASSWORD_FORMAT }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil unless user
+    user.correct_password?(password) ? user : nil
+  end
+
+  def correct_password?(password)
+    password_obj = BCrypt::Password.new(self.password_digest)
+    password_obj.is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+end
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//testing imports
+import * as SessionApiUtil from './util/session_api_util'
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  //testing
+  window.login = SessionApiUtil.login;
+  window.signup = SessionApiUtil.signup;
+  window.logout = SessionApiUtil.logout;
+
   ReactDOM.render(<h1>Welcome to POPO CRM</h1>, root);
 })
