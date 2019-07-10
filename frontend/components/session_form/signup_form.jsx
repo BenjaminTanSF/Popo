c05aa7f8e985938f79375351c99d0b7c321e4252
BenjaminTan <yiren.tan@gmail.com>
2019-07-10 11:29:25 -0700

    Add profile pic btn on signup form

--- frontend/components/session_form/signup_form.jsx
+++ frontend/components/session_form/signup_form.jsx
@@ -73,7 +73,7 @@ class SignupForm extends React.Component {
   }
 
   render() {
-    const preview = this.state.photoUrl ? <img src={this.state.photoUrl} alt="" /> : null;
+    const preview = this.state.photoUrl ? <div className="preview"><h6>Preview</h6><img className="previewPic" src={this.state.photoUrl} alt="" /></div> : null;
     return (
       <div className="signupForm">
         <div className="signupFormLeft">
@@ -152,8 +152,14 @@ class SignupForm extends React.Component {
                 />
               </label>
               <br />
-              <input type="file" id="fileBtn" onChange={this.handleFile} />
-              <h6>Preview</h6>
+              <div className="box">
+                <input type="file" name="file-2[]" id="file-2" className="inputfile inputfile-2" data-multiple-caption="{count} files selected" multiple onChange={this.handleFile} />
+                <label for="file-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z" /></svg>
+                <span>Profile Pic</span>
+                </label>
+              </div>
+              <br />
               {preview}
               <br />
               <input type="submit" value="Sign Up" id="submitBtn" />
