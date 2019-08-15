e97a416dd4cb82bd91b17ff6a4af033e695ebd6d
BenjaminTan <yiren.tan@gmail.com>
2019-08-15 16:02:53 -0700

    Change splash page background

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Splash = () => (
-  <div className="splash">
-    <h1>POPO CRM software helps you collect bribes smarter, better, faster.</h1>
-    <h3>Reach out to gangs at the right moment and engage them across every channel. POPO CRM software helps dirty cops of all shapes and sizes collect more brown envelopes the smarter way.</h3>
+  <div className="splash" style={{ backgroundImage: `url(${window.splashBgURL})` }}>
     <div className="btnDiv">
       <Link to="/login">ACCESS POPO CRM</Link>
     </div>
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -4,8 +4,8 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 //testing imports
-import { signup, login, logout } from './actions/session_actions';
-import { index, create, show, update, destroy, clearErrors } from './actions/accounts_actions';
+// import { signup, login, logout } from './actions/session_actions';
+// import { index, create, show, update, destroy, clearErrors } from './actions/accounts_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -24,19 +24,19 @@ document.addEventListener('DOMContentLoaded', () => {
     store = configureStore();
   }
 
-  //testing
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.clearErrors = clearErrors;
-  // testing accounts
-  window.index = index;
-  window.create = create;
-  window.show = show;
-  window.update = update;
-  window.destroy = destroy;
+  // //testing
+  // window.login = login;
+  // window.signup = signup;
+  // window.logout = logout;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.clearErrors = clearErrors;
+  // // testing accounts
+  // window.index = index;
+  // window.create = create;
+  // window.show = show;
+  // window.update = update;
+  // window.destroy = destroy;
 
   ReactDOM.render(<Root store={store} />, root);
 })
