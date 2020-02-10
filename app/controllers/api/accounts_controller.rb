af1a0df01211a7ca96913089f697671604fdb2cc
BenjaminTan <yiren.tan@gmail.com>
2020-02-10 15:20:35 -0800

    Beautify syntax

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 class Api::AccountsController < ApplicationController
   def index
     @accounts = current_user.accounts + current_user.supervising_accounts
-    render "api/accounts/index"
+    render 'api/accounts/index'
   end
 
   def show
     @account = Account.find(params[:id])
-    render "api/accounts/show"
+    render 'api/accounts/show'
   end
 
   def create
     @account = Account.new(account_params)
     if @account.save
-      render "api/accounts/show"
+      render 'api/accounts/show'
     else
       render json: @account.errors.full_messages, status: 422
     end
@@ -21,7 +23,7 @@ class Api::AccountsController < ApplicationController
   def update
     @account = Account.find(params[:id])
     if @account.update_attributes(account_params)
-      render "api/accounts/show"
+      render 'api/accounts/show'
     else
       render json: @account.errors.full_messages, status: 422
     end
@@ -31,13 +33,14 @@ class Api::AccountsController < ApplicationController
     @account = Account.find(params[:id])
     if @account
       @account.destroy!
-      render "api/accounts/show"
+      render 'api/accounts/show'
     else
       render json: @account.errors.full_messages, status: 422
     end
   end
 
   private
+
   def account_params
     params.require(:account).permit(:name, :industry, :website, :phone_number, :employees, :ein, :is_org, :catch_phrase, :ownership, :annual_revenue_mil, :owner_id, :sic_code, :logo)
   end
